ae4a9ca0dec0c75e8a91dd7a247bc3db9c47c05c
toyobayashi <lifenglin314@outlook.com>
2021-02-11 22:29:45 +0800

    RouterView default name

--- src/vue-router/RouterView.ts
+++ src/vue-router/RouterView.ts
@@ -17,7 +17,7 @@ import {
 } from './injectionSymbols'
 import { assign } from './utils/index'
 import { isSameRouteRecord } from './location'
-import { useSetup } from '../useSetup'
+import { defineComponent } from '../core/apiDefineComponent'
 
 export interface RouterViewProps {
   name?: string
@@ -25,92 +25,91 @@ export interface RouterViewProps {
   route?: RouteLocationNormalized
 }
 
-export const RouterView: React.FunctionComponent<RouterViewProps> = (props) => {
-  const { routeToDisplay, matchedRouteRef/* , viewRef */ } = useSetup((props) => {
-    const injectedRoute = inject<any>(routerViewLocationKey)!
-    const routeToDisplay = computed(() => props.route || injectedRoute.value)
-    const depth = inject(viewDepthKey, 0)
-    const matchedRouteRef = computed<RouteLocationMatched | undefined>(
-      () => routeToDisplay.value.matched[depth]
-    )
-
-    provide(viewDepthKey, depth + 1)
-    provide(matchedRouteKey, matchedRouteRef)
-    provide(routerViewLocationKey, routeToDisplay)
-
-    const viewRef = ref<any>()
-
-    // watch at the same time the component instance, the route record we are
-    // rendering, and the name
-    watch(
-      () => [viewRef.value, matchedRouteRef.value, props.name] as const,
-      ([instance, to, name], [oldInstance, from, _oldName]) => {
-        // copy reused instances
-        if (to) {
-          // this will update the instance for new instances as well as reused
-          // instances when navigating to a new route
-          to.instances[name!] = instance
-          // the component instance is reused for a different route or name so
-          // we copy any saved update or leave guards
-          if (from && from !== to && instance && instance === oldInstance) {
-            to.leaveGuards = from.leaveGuards
-            to.updateGuards = from.updateGuards
-          }
-        }
-
-        // trigger beforeRouteEnter next callbacks
-        if (
-          instance &&
-          to &&
-          // if there is no instance but to and from are the same this might be
-          // the first visit
-          (!from || !isSameRouteRecord(to, from) || !oldInstance)
-        ) {
-          ;(to.enterCallbacks[name!] || []).forEach(callback =>
-            callback(instance)
-          )
-        }
-      },
-      { flush: 'post' }
-    )
-
-    return { routeToDisplay, matchedRouteRef, viewRef }
-  }, props)
-
-  const route = routeToDisplay.value
-  const matchedRoute = matchedRouteRef.value
-  const ViewComponent = matchedRoute && matchedRoute.components[props.name!]
-
-  // we need the value at the time we render because when we unmount, we
-  // navigated to a different location so the value is different
-  // const currentName = props.name
-
-  if (!ViewComponent) {
-    return React.createElement(React.Fragment, null, props.children)
-  }
-
-  // props from route configuration
-  const routePropsOption = matchedRoute!.props[props.name!]
-  const routeProps = routePropsOption
-    ? routePropsOption === true
-      ? route.params
-      : typeof routePropsOption === 'function'
-      ? routePropsOption(route)
-      : routePropsOption
-    : null
-
-  // const onVnodeUnmounted: VNodeProps['onVnodeUnmounted'] = vnode => {
-  //   // remove the instance reference to prevent leak
-  //   if (vnode.component!.isUnmounted) {
-  //     matchedRoute!.instances[currentName] = null
-  //   }
-  // }
-
-  return React.createElement(
-    ViewComponent,
-    assign({}, routeProps, props, {
-      // onVnodeUnmounted,
-      // ref: viewRef,
-    })
+export const RouterView = defineComponent<RouterViewProps>((props) => {
+  const name = computed(() => ('name' in props) ? props.name : 'default')
+  const injectedRoute = inject<any>(routerViewLocationKey)!
+  const routeToDisplay = computed(() => props.route || injectedRoute.value)
+  const depth = inject(viewDepthKey, 0)
+  const matchedRouteRef = computed<RouteLocationMatched | undefined>(
+    () => routeToDisplay.value.matched[depth]
   )
-}
+
+  provide(viewDepthKey, depth + 1)
+  provide(matchedRouteKey, matchedRouteRef)
+  provide(routerViewLocationKey, routeToDisplay)
+
+  const viewRef = ref<any>()
+
+  // watch at the same time the component instance, the route record we are
+  // rendering, and the name
+  watch(
+    () => [viewRef.value, matchedRouteRef.value, name.value] as const,
+    ([instance, to, name], [oldInstance, from, _oldName]) => {
+      // copy reused instances
+      if (to) {
+        // this will update the instance for new instances as well as reused
+        // instances when navigating to a new route
+        to.instances[name!] = instance
+        // the component instance is reused for a different route or name so
+        // we copy any saved update or leave guards
+        if (from && from !== to && instance && instance === oldInstance) {
+          to.leaveGuards = from.leaveGuards
+          to.updateGuards = from.updateGuards
+        }
+      }
+
+      // trigger beforeRouteEnter next callbacks
+      if (
+        instance &&
+        to &&
+        // if there is no instance but to and from are the same this might be
+        // the first visit
+        (!from || !isSameRouteRecord(to, from) || !oldInstance)
+      ) {
+        ;(to.enterCallbacks[name!] || []).forEach(callback =>
+          callback(instance)
+        )
+      }
+    },
+    { flush: 'post' }
+  )
+  
+  return () => {
+    const route = routeToDisplay.value
+    const matchedRoute = matchedRouteRef.value
+    const ViewComponent = matchedRoute && matchedRoute.components[name.value!]
+
+    // we need the value at the time we render because when we unmount, we
+    // navigated to a different location so the value is different
+    // const currentName = props.name
+
+    if (!ViewComponent) {
+      return React.createElement(React.Fragment, null, props.children)
+    }
+
+    // props from route configuration
+    const routePropsOption = matchedRoute!.props[name.value!]
+    const routeProps = routePropsOption
+      ? routePropsOption === true
+        ? route.params
+        : typeof routePropsOption === 'function'
+        ? routePropsOption(route)
+        : routePropsOption
+      : null
+
+    // const onVnodeUnmounted: VNodeProps['onVnodeUnmounted'] = vnode => {
+    //   // remove the instance reference to prevent leak
+    //   if (vnode.component!.isUnmounted) {
+    //     matchedRoute!.instances[currentName] = null
+    //   }
+    // }
+
+    return React.createElement(
+      ViewComponent,
+      assign({}, routeProps, props, {
+        // onVnodeUnmounted,
+        // ref: viewRef,
+      })
+    )
+  }
+}, 'RouterView')
