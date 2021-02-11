de73d08b521d1de36a8feeebc41d71b1b6b46eb9
toyobayashi <lifenglin314@outlook.com>
2021-02-11 22:30:24 +0800

    test

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { HashRouter, Switch, Route, Link } from 'react-router-dom'
-import { defineAsyncComponent } from '../..'
+// import { /* HashRouter, Switch, Route, Link */ } from 'react-router-dom'
+import { defineAsyncComponent, createRouter, createWebHashHistory, RouterView, RouterLink } from '../..'
 
 const MarkdownView = defineAsyncComponent(() => import('./components/MarkdownView'))
 const GithubCommitView = defineAsyncComponent(() => import('./components/GithubCommitView'))
@@ -8,26 +8,61 @@ const GridComponent = defineAsyncComponent(() => import('./components/GridCompon
 
 const Home: React.FunctionComponent = () => {
   return (
-    <>
-      <div><Link to="/markdown">markdown</Link></div>
-      <div><Link to="/github">github</Link></div>
-      <div><Link to="/grid">grid</Link></div>
-    </>
+    <div>
+      <div><RouterLink to={{ name: 'markdown' }}>markdown</RouterLink></div>
+      <div><RouterLink to={{ name: 'github' }}>github</RouterLink></div>
+      <div><RouterLink to={{ name: 'grid' }}>grid</RouterLink></div>
+      <RouterView />
+    </div>
   )
 }
+Home.displayName = 'Home'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      children: [
+        {
+          path: 'markdown',
+          name: 'markdown',
+          component: () => Promise.resolve(MarkdownView)
+        },
+        {
+          path: 'github',
+          name: 'github',
+          component: () => Promise.resolve(GithubCommitView)
+        },
+        {
+          path: 'grid',
+          name: 'grid',
+          component: () => Promise.resolve(GridComponent)
+        }
+      ]
+    }
+  ]
+})
+router.install()
 
 const App: React.FunctionComponent = () => {
   return (
     <>
-      <HashRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/markdown" exact component={MarkdownView} />
-          <Route path="/github" exact component={GithubCommitView} />
-          <Route path="/grid" exact component={GridComponent} />
-        </Switch>
-      </HashRouter>
+      <RouterView />
+      
     </>
+    // <>
+    //   <HashRouter>
+    //     <Switch>
+    //       <Route path="/" exact component={Home} />
+    //       <Route path="/markdown" exact component={MarkdownView} />
+    //       <Route path="/github" exact component={GithubCommitView} />
+    //       <Route path="/grid" exact component={GridComponent} />
+    //     </Switch>
+    //   </HashRouter>
+    // </>
   )
 }
 
